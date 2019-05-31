b1acf71f6d938798e3d7d505f475d39a97baf6a2
minnan0328 <minnan0328@gmail.com>
2019-05-31 21:26:46 +0800

    移除

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <div>PWA test</div>
-    <img src="./../static/img/PWA.png" style="width:100%">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
